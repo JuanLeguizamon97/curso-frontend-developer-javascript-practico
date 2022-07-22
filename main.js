51626b48f2db48d280311d4e7454c361aa4ab4c8
Juan Leguizamon <juanc.leguizamon@urosario.edu.co>
2022-07-22 18:39:09 -0500

    Integración del menu responsive y el menu desktop con funcionalidad. Además, integración del carrito de compras para ver el resumen del pedido

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuShoppingCart = document.querySelector('.navbar-shopping-cart');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingDetail = document.querySelector('.product-detail')
+
 
 navEmail.addEventListener('click', toogleDesktopMenu);
+burgerMenu.addEventListener('click', toogleMobileMenu);
+menuShoppingCart.addEventListener('click', toogleShoppingDetail);
 
 function toogleDesktopMenu(){
+    const isShoppingCartClosed= shoppingDetail.classList.contains('inactive');
+
+    if (!isShoppingCartClosed){
+        shoppingDetail.classList.add('inactive');
+    }
+
     desktopMenu.classList.toggle('inactive');
 }
+
+function toogleMobileMenu(){
+    const isShoppingCartClosed= shoppingDetail.classList.contains('inactive');
+
+    if (!isShoppingCartClosed){
+        shoppingDetail.classList.add('inactive');
+    }
+
+    mobileMenu.classList.toggle('inactive');
+
+}
+
+function toogleShoppingDetail(){
+    const isMobileMenuClosed= mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    
+    shoppingDetail.classList.toggle('inactive');
+}
+
+//Product list consult
+const productList =[];
